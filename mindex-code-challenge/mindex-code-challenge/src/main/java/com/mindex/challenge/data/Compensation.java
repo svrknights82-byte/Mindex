f93447bad323d76a5ec35a1e224ccebea965b990
Samuel Roberts <svr9047@g.rit.edu>
2025-08-13 20:13:31 -0400

    Built dao and data for Compensation

---
+++ mindex-code-challenge/mindex-code-challenge/src/main/java/com/mindex/challenge/data/Compensation.java
@@ -0,0 +1,42 @@
+package com.mindex.challenge.data;
+
+import java.time.LocalDate;
+
+public class Compensation {
+    private String employeeId; // This is how we will link this to an employee
+    private double salary;
+    private LocalDate effectiveDate;
+
+    public Compensation() {
+    }
+
+    public Compensation(String employeeId, double salary, LocalDate effectiveDate) {
+        this.employeeId = employeeId;
+        this.salary = salary;
+        this.effectiveDate = effectiveDate;
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public LocalDate getEffectiveDate() {
+        return effectiveDate;
+    }
+
+    public void setEffectiveDate(LocalDate effectiveDate) {
+        this.effectiveDate = effectiveDate;
+    }
+}
